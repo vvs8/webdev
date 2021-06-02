b6b42d019b45398a818979095099490eacfda198
Vadym Shakhraichuk <shakhraichuk@gmail.com>
2021-06-01 22:19:05 -0700

    Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import DishDetail from './components/DishdetailComponent';
-import './App.css';
-import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
-
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
    
    render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+	    <BrowserRouter>
+		    <div className="App">
+		    	<Main />
+		    </div>
+	    </BrowserRouter>
     );
   }
 }
