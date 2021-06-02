6a91c5e7d2578f324d4b5be7772443bf26bdc921
Vadym Shakhraichuk <shakhraichuk@gmail.com>
2021-06-01 22:51:37 -0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 
     function RenderDish({dish}) {
 
         return(
-            <div class="col-12 col-md-5 m-1">
+            <div className="col-12 col-md-5 m-1">
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
@@ -19,25 +22,22 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     function RenderComments({comments}) {
-
         if (comments != null)
-        return(
-            comments.map((comment)=>{
-                return (
-                
-                <React.Fragment>
-                    <div key={comment.id}>
-                        
-                        <ul className="list-unstyled">
-                            <li>{comment.comment}</li>
-                            <li>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                        </ul>
-                    </div>
-                    
-                </React.Fragment>
-                )
-            })
-        );
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">   
+                        {comments.map((comment)=> {
+                            return (
+                                <li key ={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>   
+                            );
+                        })}       
+                    </ul>    
+                </div>    
+            );
         else
             return(
                 <div></div>
@@ -47,20 +47,28 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
     const DishDetail = (props) => { 
         if (props.dish != null)
-            return ( 
-                <div class = "container">
-                    <div class = "row">
-                           
-                            <RenderDish dish = {props.dish} />
-                            
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
                         
-                        <div class="col-12 col-md-5 m-1">
-                            <h3>Comments</h3>
-                            <RenderComments comments = {props.dish.comments} />
-                        </div>
-                    </div> 
-                </div>  
-            )
+                            <RenderDish dish={props.dish} />
+                       
+                       
+                            <RenderComments comments={props.comments} />
+                      
+                    </div>
+                </div>
+            );
         else
             return(
                 <div></div>
